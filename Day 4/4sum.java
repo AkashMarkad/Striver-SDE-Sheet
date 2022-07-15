463593e046838037ac4b71e859dd1572fe3b0aa0
AkashMarkad <79374621+AkashMarkad@users.noreply.github.com>
2022-07-15 21:27:50 +0530

    Update 4sum.java

--- Day 4/4sum.java
+++ Day 4/4sum.java
@@ -80,3 +80,51 @@ class Solution {
         return res;
     }
 }
+
+
+// Same Solution of Given Problem on Coding Studio 
+// Problem Link : https://www.codingninjas.com/codestudio/problems/983605?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
+public class Solution {
+  public static String fourSum(int[] arr, int target, int n) {
+      // Write your code here.
+      
+      if(arr == null || arr.length == 0)
+          return "No";
+      
+      Arrays.sort(arr);
+      
+      for(int i=0;i<n;i++)
+      {
+          for(int j=i+1;j<n;j++)
+          {
+              long target_2 = (long)target - (long)arr[i] - (long)arr[j];
+              
+              int left = j+1;
+              int right = n-1;
+              
+              while(left < right)
+              {
+                  long two_sum = (long)arr[left] + (long)arr[right];
+                  
+                  if(two_sum < target_2)    left++;
+                  else if(two_sum > target_2)    right--;
+                  else
+                  {
+                      return "Yes";
+                  }
+              }
+              
+//               Processing the duplicates of number 2
+              while(j+1 < n && arr[j+1]== arr[j])    j++;
+          }
+          
+//           Processing the duplicates of number 1
+          while(i+1 < n && arr[i+1] == arr[i])    i++;
+          
+      }
+      
+      return "No";
+      
+  }
+}
