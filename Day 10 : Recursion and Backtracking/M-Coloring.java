4bded3680006333a139b4003bf8f3f9b3bc0690a
AkashMarkad <79374621+AkashMarkad@users.noreply.github.com>
2022-08-05 01:18:30 +0530

    Create M-Coloring.java

---
+++ Day 10 : Recursion and Backtracking/M-Coloring.java
@@ -0,0 +1,12 @@
+// M-Coloring Problem 
+
+// We need color the graph at max given color so that no adjacent node have the same color 
+// If Possible then return 1, else return 0
+
+// Problem Link : 1) https://practice.geeksforgeeks.org/problems/m-coloring-problem-1587115620/1#
+//                2) https://www.codingninjas.com/codestudio/problems/981273?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+
+// Approach : 
+
+// Code :
+
