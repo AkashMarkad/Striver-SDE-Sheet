31763a3c808d481ad2c6ac90913bb35a782f07d6
AkashMarkad <79374621+AkashMarkad@users.noreply.github.com>
2022-08-30 15:04:03 +0530

    Create Minimum Character for Palindrome.java

---
+++ Day 16 : String Part-II/Minimum Character for Palindrome.java
@@ -0,0 +1,53 @@
+// Minimum Characters For Palindrome
+// Input: str = "abcd";
+// Output: 3
+
+// Approach 1: Brute Force
+// delete the character at the end and check the string is palindrome or not
+// continuoulsy delete the character from the back until string is palindrome or string becomes null
+// return the number of character is deleted
+
+// Time Complexity - O(n*n)
+// Space Complexity - O(1)
+
+// Approach 2: Using KMP Algorithm
+// str = s+ "$" + s.reverse();
+// find the lps array for the above string 
+// and return the length of s - last element of the lps array
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+// Code:
+
+public class Solution {
+
+    public static int[] generateLPS(String ns , int n){
+        
+        int[] lps = new int[n];
+        for(int i=1;i<n;i++){
+            int j =lps[i-1];
+            while( j > 0 && ns.charAt(i) != ns.charAt(j))
+                j = lps[j-1];
+            
+            if(ns.charAt(i)==ns.charAt(j))    j++;
+            lps[i]=j;
+        }
+        
+        return lps;
+    }
+    
+    public static int minCharsforPalindrome(String str) {
+        //Write your code here
+        
+        StringBuilder sb = new StringBuilder(str);
+        
+        sb.reverse();
+        String new_str = str + "$" + sb.toString();
+        int n = new_str.length();
+        int[] lps = generateLPS(new_str , n);
+        
+        return str.length()-lps[n-1];
+    }
+
+}
