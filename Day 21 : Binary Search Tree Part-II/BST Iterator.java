2b5d7f56c74da008273408b4143ef4301d81c966
AkashMarkad <79374621+AkashMarkad@users.noreply.github.com>
2022-10-01 18:46:46 +0530

    Create BST Iterator.java

---
+++ Day 21 : Binary Search Tree Part-II/BST Iterator.java
@@ -0,0 +1,51 @@
+// BST Iterator 
+
+// BST Iterator Returns data in incresing order
+// It has following functions: 1) hsNext(), 2) next(), 3) pushAll(), 4) Constructor
+
+// Code:
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+
+    private Stack<TreeNode> st = new Stack<>();
+
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+    
+    public int next() {
+        TreeNode temp = st.pop();
+        pushAll(temp.right);
+        return temp.val;
+    }
+    
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+
+    private void pushAll(TreeNode node){
+        for(; node != null ; st.push(node) , node = node.left);
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
