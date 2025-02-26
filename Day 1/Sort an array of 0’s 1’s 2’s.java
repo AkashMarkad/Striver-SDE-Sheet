d4e61a296cdfd5f17409595e294851d2cf931329
Akash Markad <79374621+AkashMarkad@users.noreply.github.com>
2025-02-26 22:49:50 +0530

    Update Sort an array of 0’s 1’s 2’s.java

--- Day 1/Sort an array of 0’s 1’s 2’s.java
+++ Day 1/Sort an array of 0’s 1’s 2’s.java
@@ -1,7 +1,7 @@
 // We can solve this problem using three ways 
 // 1. By Brute Force
 // 2. By Counting sort - Count number of 0's, 1's and 2's and according to place the elements in the array
-// 3. By Dutch National Flag Algorithm - It's Most optimal way to solve the dutch 012 sort. In this algorithm we take low, mid and high pointers. Initially low and mid assighned to 0 index and high assigned to the last index. And swap if mid at 0 then with the 0 and if mid at 2 then with high And if mid is 1 then increase mid by 1.
+// 3. By Dutch National Flag Algorithm - It's Most optimal way to solve the dutch 012 sort. In this algorithm we take low, mid and high pointers. Initially low and mid assighned to 0 index and high assigned to the last index. And swap if mid at 0 then with the low and if mid at 2 then with high And if mid is 1 then increase mid by 1.
 
 //Dutch National Flag Algorithm
 public class Solution 
